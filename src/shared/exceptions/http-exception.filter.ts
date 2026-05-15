dc24c183539d979c3d958c4466d4a4e6214e00ed
Mohamed-Shahata <mohamedmrslan@gmail.com>
2026-05-16 02:31:02 +0300

    rebase update exception

--- src/shared/exceptions/http-exception.filter.ts
+++ src/shared/exceptions/http-exception.filter.ts
@@ -14,27 +14,19 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
   catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
-
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
-    let message = 'Internal server error';
+    let message: string | string[] = 'Internal server error';
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
-
       const res = exception.getResponse();
-
-      if (typeof res === 'string') {
-        message = res;
-      } else if (typeof res === 'object' && res && 'message' in res) {
-        const msg = (res as any).message;
-
-        message = Array.isArray(msg) ? msg.join(', ') : String(msg);
-      } else {
-        message = exception.message;
-      }
+      message =
+        typeof res === 'object' && 'message' in res
+          ? (res as any).message
+          : exception.message;
     } else {
       this.logger.error('Unhandled exception', exception as Error);
     }
