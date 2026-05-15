4675c251b2bca87baa47b0b536821d94d649d209
Mohamed-Shahata <mohamedmrslan@gmail.com>
2026-05-16 02:09:47 +0300

    Unifying the error message

--- src/shared/exceptions/http-exception.filter.ts
+++ src/shared/exceptions/http-exception.filter.ts
@@ -14,19 +14,27 @@ export class GlobalExceptionFilter implements ExceptionFilter {
 
   catch(exception: unknown, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
+
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
-    let message: string | string[] = 'Internal server error';
+    let message = 'Internal server error';
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
+
       const res = exception.getResponse();
-      message =
-        typeof res === 'object' && 'message' in res
-          ? (res as any).message
-          : exception.message;
+
+      if (typeof res === 'string') {
+        message = res;
+      } else if (typeof res === 'object' && res && 'message' in res) {
+        const msg = (res as any).message;
+
+        message = Array.isArray(msg) ? msg.join(', ') : String(msg);
+      } else {
+        message = exception.message;
+      }
     } else {
       this.logger.error('Unhandled exception', exception as Error);
     }
