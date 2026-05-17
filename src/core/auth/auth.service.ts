4574fd0ceac1208f28d323bed0ab77ead7dc0160
Mohamed-Shahata <mohamedmrslan@gmail.com>
2026-05-17 05:50:39 +0300

    rebase

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -188,11 +188,9 @@ export class AuthService {
         const waitSeconds = Math.ceil(
           (RESEND_COOLDOWN_MINUTES - minutesSince) * 60,
         );
-        throw new BadRequestException({
-          message: `Please wait ${waitSeconds} seconds before requesting a new code`,
-          waitSeconds,
-          error: 'OTP_RATE_LIMIT',
-        });
+        throw new BadRequestException(
+          `Please wait ${waitSeconds} seconds before requesting a new code`,
+        );
       }
     }
 
