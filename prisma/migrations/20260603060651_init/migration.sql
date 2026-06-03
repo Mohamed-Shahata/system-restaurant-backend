f7afeb27fed44c0c5750c7d45f82e7831df7aeac
Mohamed-Shahata <mohamedmrslan@gmail.com>
2026-06-03 09:10:50 +0300

    init migrate

---
+++ prisma/migrations/20260603060651_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "favorites" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "menu_item_addons" ALTER COLUMN "id" DROP DEFAULT;
+
+-- AlterTable
+ALTER TABLE "menu_item_sizes" ALTER COLUMN "id" DROP DEFAULT;
